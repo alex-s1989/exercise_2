ffa9c9537d4184380db56be0266222456a98b067
alex <shu5388369@gmail.com>
2018-04-05 21:13:58 +0300

    Remove idea and correction task_5

--- task_5.rb
+++ task_5.rb
@@ -7,13 +7,13 @@ month = gets.chomp.to_i
 print "Введите год "
 year = gets.chomp.to_i
 
-days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+days = [ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 
 # проверяем високосный ли год
 
 leap_year = ( year % 4 == 0 && year % 100 != 0 ) || ( year % 400 == 0)
-days[1] = 29 if leap_year
+days[2] = 29 if leap_year
 
-sum = days[0..month-2].sum
+sum = days[0..month-1].sum
 
 puts "Порядковый номер дня в году:  #{sum + day}"
